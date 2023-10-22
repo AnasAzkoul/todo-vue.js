f86df3c94f973373b0ab2c1c97d6cacaf3790268
Anas azkoul <anas@Anass-MacBook-Air.local>
2023-10-22 20:44:29 +0200

    first Vue.js lesson

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,23 @@
+<script setup>
+  import { defineProps } from 'vue';
+  const props = defineProps({
+    todo: {
+      type: Object,
+      required: true,
+    }
+  })
+</script>
+<template>
+  <li>
+    <input type="checkbox" :checked="todo.isCompleted">
+    <div class="todo">
+      <input type="text" :value="todo.todo">
+      <span>
+        {{ todo.todo }}
+      </span>
+    </div>
+    <div class="todo-actions"></div>
+  </li>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/views/TodosView.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { ref } from 'vue'
+import { uid } from 'uid'
+import TodoCreator from '../components/TodoCreator.vue'
+import TodoItem from '../components/TodoItem.vue'
+
+const todosList = ref([])
+
+const createTodo = (todo) => {
+  todosList.value.push({
+    id: uid(),
+    todo,
+    isCompleted: null,
+    isEditing: null
+  })
+}
+</script>
+
+<template>
+  <main>
+    <h1>Create Todo</h1>
+    <TodoCreator v-on:create-todo="createTodo" />
+    <ul>
+      <TodoItem v-for="todo in todosList" v-bind:key="todo.id" :todo="todo" />
+    </ul>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  max-width: 500px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 40px 16px;
+
+  h1 {
+    margin-bottom: 16px;
+    text-align: center;
+  }
+}
+</style>
